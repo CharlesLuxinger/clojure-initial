c9d1c52c80294b174f1ac16a545867dc96b6bbc4
CharlesLuxinger <charlesluxinger@gmail.com>
2021-09-02 09:06:14 -0300

    Add Datomic

--- ecommerce/project.clj
+++ ecommerce/project.clj
@@ -1,7 +1,10 @@
-(defproject ecommerce "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject ecommerce "1.0.0"
+  :description "Project to learning Datomic"
+  :url "https://github.com/CharlesLuxinger/clojure-initial"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.datomic/datomic-pro "1.0.6316"]]
   :repl-options {:init-ns ecommerce.core})
