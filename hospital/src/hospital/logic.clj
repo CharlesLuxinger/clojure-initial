d61a2b1f559f3c60fe9175b5ffdea58faadc898a
CharlesLuxinger <charlesluxinger@gmail.com>
2021-09-10 17:19:34 -0300

    Global Var - Concurrent Thread Error

--- hospital/src/hospital/logic.clj
+++ hospital/src/hospital/logic.clj
@@ -1,8 +1,23 @@
 (ns hospital.logic)
 
+(defn queue-limit-size? [hospital department]
+  (-> hospital
+      (get department)
+      count
+      (< 5)))
+
 (defn come-in
   [hospital department person]
-  (update hospital department conj person))
+  (if (queue-limit-size? hospital department)
+    (update hospital department conj person)
+    (throw (ex-info "Queue is full" {:trying-add person}))))
+
+(defn come-in-sleep
+  [hospital department person]
+  (if (queue-limit-size? hospital department)
+    (do (Thread/sleep 1000)
+        (update hospital department conj person))
+    (throw (ex-info "Queue is full" {:trying-add person}))))
 
 (defn call
   [hospital department]
--- hospital/src/hospital/queue_simulation.clj
+++ hospital/src/hospital/queue_simulation.clj
@@ -19,4 +19,22 @@
   (def hospital (h.logic/call hospital :wait))
   (pprint hospital))
 
-(queue-simulation)
+;(queue-simulation)
+
+(defn come-in-the-devil [person]
+  (def hospital (h.logic/come-in-sleep hospital :wait person))
+  (println "after insert:" person))
+
+(defn queue-simulation-in-parallel
+  []
+  (def hospital (h.model/new-empty-queue))
+  (.start (Thread. (fn [] (come-in-the-devil "111"))))
+  (.start (Thread. (fn [] (come-in-the-devil "222"))))
+  (.start (Thread. (fn [] (come-in-the-devil "333"))))
+  (.start (Thread. (fn [] (come-in-the-devil "444"))))
+  (.start (Thread. (fn [] (come-in-the-devil "555"))))
+  (.start (Thread. (fn [] (come-in-the-devil "666"))))
+  (.start (Thread. (fn [] (Thread/sleep 4000)
+                     (pprint hospital)))))
+
+(queue-simulation-in-parallel)
